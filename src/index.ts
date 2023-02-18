382d44185d434b88f3b144a1682fdcf85ed9159b
Marciodev2000 <marciodev200@gmail.com>
2023-02-18 16:00:43 +0100

    estilo de codigo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 import { servidor } from './server/server';
-
 servidor.listen('3333', ()=> console.log('Servidor Ligado'));
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,11 +2,8 @@ import express from 'express';
 
 const servidor = express();
 
-servidor.get('/', (req, res) =>{
+servidor.get('/', (req, res) => {
   return  res.send('Rota Principal');
-})
+});
 
-
-
-
-export {servidor}
+export {servidor};
